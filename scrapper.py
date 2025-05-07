d310f3313d0fe100d6acfb16611ff5fafc8b6732
NarimanMammadov <nmammadov16237@ada.edu.az>
2025-05-07 23:58:30 +0400

    collecting all links

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,19 @@
 from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import time
 
-url = "https://bina.az/items/all"
+base_url = "https://bina.az/items/all"
 headers = {'User-Agent': 'Mozilla/5.0'}
 
-req = Request(url, headers=headers)
-page = urlopen(req)
-content = page.read()
+def get_links_from_base():
+    req = Request(base_url, headers=headers)
+    soup = BeautifulSoup(req.content, "html.parser")
 
-print(content)
+    links = []
+    for a in soup.find_all("a", class_="slider_controls"):
+        links.append(a["href"])
+    return links
+
+def main():
+    links = get_links_from_base()
+    print(links)
